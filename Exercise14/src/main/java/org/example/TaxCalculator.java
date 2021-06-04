6a7e14a1a6bf62ea2eaa398498f3aebb74cbc94b
jonathan-navarr <84359494+jonathan-navarr@users.noreply.github.com>
2021-06-03 23:31:11 -0400

    Add files via upload

---
+++ Exercise14/src/main/java/org/example/TaxCalculator.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double subtotal, tax, total;
+
+        System.out.print("What is the order amount? ");
+        subtotal = scnr.nextInt();
+        System.out.print("What is the state? ");
+        String state = scnr.next();
+        if (state.equals("WI")){
+            tax = (.055*subtotal);
+        }
+        else{
+            tax = (0);
+        }
+
+        String strSubtotal = String.format("%.2f", subtotal);
+        String strTax = String.format("%.2f", tax);
+        total = (subtotal + tax);
+        String strTotal = String.format("%.2f", total);
+
+        System.out.print("The subtotal is $" + strSubtotal + "\nThe tax is $" +strTax + "\nThe total is $" + strTotal);
+
+
+
+    }
+}
